34adb60a7818426c3cd744ce8488603967f3a64b
10957393 <103188479+JEREMIAH-LAVOE@users.noreply.github.com>
2022-08-08 13:35:51 +0100

    Add files via upload

---
+++ JEREMIAH LAVOE 10957393/ASSIGNMENT.cpp
@@ -0,0 +1,29 @@
+// Program to find the sum of prime numbers up to a given number
+#include <iostream>
+using namespace std;
+int main () 
+{
+    int num, i, upto, sum = 0;
+    cout << "Find the sum of prime numbers upto :";
+    cin >> upto;
+    
+    for(num = 2; num <= upto; num++)
+     {
+    for(i = 2; i <= (num /2); i++)
+     {
+    if(num % i ==0)
+    {
+        i = num;
+        break;
+         }
+    }
+    if (i != num)
+    {
+        sum += num;
+    }
+
+    }
+    cout << endl << "Sum of all prime numbers upto " << upto << ":" << sum;
+    return 0;
+
+}
